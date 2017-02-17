e556c5a67aa827a73204a68b53bd1a78bac8de23
roydejager <roydejager@live.nl>
2017-02-17 18:58:59 +0100

    attempt to fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,36 @@
 var path = require('path')
 var webpack = require('webpack')
-
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
 module.exports = {
-    entry: [
-        __dirname + '/src/index.js'
-    ],
-    output: {
-        filename: 'bundle.js',
-        path:  __dirname + '/dist',
-    },
-    module: {
-        loaders: [
-            {
-                test: /.js?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query: {
-                    presets: ['es2015', 'react']
-                }
-            },
-            {
-                test: /\.css$/,
-                loaders: [
-                'style-loader',
-                'css-loader?importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
-                'postcss-loader'
-                ]
-            }
-        ]
-    },
-    plugins: [
-        require('postcss-cssnext')
-    ]
+  entry:
+     SRC_DIR + '/index.js',
+  output: {
+      path: DIST_DIR + '/',
+      filename: 'bundle.js',
+      publicpath: '/'
+  },
+  module: {
+      loaders: [
+          {
+              test: /\.js?$/,
+              include: SRC_DIR,
+              loader: 'babel-loader',
+              exclude: /node_modules/,
+              query: {
+                  presets: ['es2015', 'react']
+              }
+          },
+          {
+              test: /\.css$/,
+              loaders: [
+              'style-loader',
+              'css-loader?importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+              'postcss-loader'
+              ]
+          }
+      ]
+  },
+  plugins: [require('postcss-cssnext')
+  ]
 }
