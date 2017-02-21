e50cc676a76433678f98864f8526c3ab99ff386e
roydejager <roydejager@live.nl>
2017-02-21 09:33:25 +0100

    Add postcss import

--- src/js/components/blocks/app.js
+++ src/js/components/blocks/app.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import Block from './block.js'
 import Style from './style.css'
+import Styling from './styling.css'
 
 class test extends React.Component {
     render() {
         return (
           <div>
-            <div className={Style.test}>Hello world!</div>
-            <div className="test">kwehjfbewk</div>
+            <div className={Style.test}>
+               Hello world!
+               <p>hello back</p>
+            </div>
+            <Block/>
+            <div className={Styling.testTwo}>kwehjfbewk</div>
           </div>
         )
     }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path')
 var webpack = require('webpack')
-var DIST_DIR = path.resolve(__dirname, "dist");
-var SRC_DIR = path.resolve(__dirname, "src");
+var DIST_DIR = path.resolve(__dirname, "dist")
+var SRC_DIR = path.resolve(__dirname, "src")
 
 module.exports = {
   entry:
