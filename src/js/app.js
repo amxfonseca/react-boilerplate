87a184c8664cc49ce51ef0f8f55f507d6d4906c6
roydejager <roydejager@live.nl>
2017-02-22 10:02:09 +0100

    add more routing

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+import React from 'react' 
+
+function App(props) {
+  const childrenWithProps = props
+
+  return(
+    <div>
+      <div>oegaboega</div>
+      {childrenWithProps}
+    </div>
+  )
+}
+
+export default App
--- src/js/routing/routes.js
+++ src/js/routing/routes.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import { Router, Route, hashHistory } from 'react-router'
-import App from './../components/blocks/app'
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+import App from '../app'
 import Block from './../components/blocks/block'
+import Appie from './../components/blocks/app'
+
 
 function Routes() {
   return(
     <Router history={hashHistory}>
-      <Route path="/" component={App}/>
-      <Route path="/block" component={Block}/>
+      <Route path="/" component={App}>
+        <IndexRoute component={Appie}/>
+        <Route path="/block" component={Block}/>
+        <Route path="/appie" component={Appie}/>
+      </Route>
     </Router>
   )
 }
