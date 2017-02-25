88491f5d9a8186d1febe781afc0d0a3d2103982d
roydejager <roydejager@live.nl>
2017-02-25 01:59:09 +0100

    add css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Routes from './js/routing/routes'
 
+import './style/main.css'
 
 ReactDOM.render(
   <Routes/>, document.getElementById('root')
