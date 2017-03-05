21ad81cdf4a63be81c6a4ba4ebb79353ddb6cc0f
roydejager <roydejager@live.nl>
2017-03-05 01:03:33 +0100

    add ejs

--- app/index.js
+++ app/index.js
@@ -5,14 +5,15 @@ const port = process.env.PORT || 3000
 
 const app = express()
 
+app.set('view engine', 'ejs')
+app.set('views', './views')
 
 app.use(require('./middleware'))
 
 
 app.get('*', function (request, response){
-    response.sendFile(path.resolve(__dirname, './views', 'index.html'))
+    response.render(path.resolve(__dirname, './views', 'index.ejs'))
 })
-
 
 app.listen(port)
 console.log("server started on port " + port)
